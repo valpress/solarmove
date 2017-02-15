26d9079b4b0ac43b8b593b81bb81cffbe987518b
sportmaps <repository@sportmaps.ch>
2017-02-15 18:21:33 +0100

    legal mentions

---
+++ sections/lm/legal-mentions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * @package WordPress
+ * @subpackage Integral
+ * @since Integral 1.0
+ */
+?>
+<?php global $integral; ?>
+<section id="legal-mentions" class="legal-mentions" style="margin-top:60px;">
+    <div class="container">
+		<div class="row">
+            <div class="col-md-12">
+	            <h1><?php echo __($integral['legal-mentions-title'], 'solarmove'); ?></h1>
+	            <?php echo __($integral['legal-mentions-text'], 'solarmove'); ?>
+	        </div>
+		</div>
+	</div>
+</section>
